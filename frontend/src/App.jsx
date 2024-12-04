22cc7a23fa192da6b8e36f4988975eaaab77f4b3
Amit Sarkar <mroxygen2000@gmail.com>
2024-12-04 21:53:01 +0530

    Add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,13 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Dashboard from './pages/Dashboard'
 import NewsArticles from './pages/NewsArticles'
+import Header from './components/shared/Header'
 
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
      <Routes>
       <Route path='/sign-up' element={<SignupForm/>} />
       <Route path='/sign-in' element={<SigninForm/>}/>
