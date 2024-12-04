177a96ef4ba769c334405a789dfbe21567e0a1b0
Amit Sarkar <mroxygen2000@gmail.com>
2024-12-04 21:13:46 +0530

    Add pages and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
-import { Button } from './components/ui/button'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import SignupForm from './auth/forms/SignupForm'
+import SigninForm from './auth/forms/SigninForm'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import NewsArticles from './pages/NewsArticles'
+
 
 const App = () => {
   return (
-    <div className="text-3xl  underline text-yellow-400">
-      Hello world!
-      <Button>Click me</Button>
-    </div>
+    <BrowserRouter>
+     <Routes>
+      <Route path='/sign-up' element={<SignupForm/>} />
+      <Route path='/sign-in' element={<SigninForm/>}/>
+
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/news' element={<NewsArticles/>}/>
+     </Routes>
+    </BrowserRouter>
   )
 }
 
