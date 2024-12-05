b6b308145bb2e1a26494a73e1cba376845221897
Amit Sarkar <mroxygen2000@gmail.com>
2024-12-05 23:05:06 +0530

    create sihn api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs"
 import User from "../model/user.model.js"
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken"
 
 export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
@@ -22,5 +23,30 @@ export const signup=async(req,res,next)=>{
         res.json("Signup seccessfull!");
     } catch (error) {
         return next(error);
+    }
+}
+export const signin=async(req,res,next)=>{
+    const {email,password}=req.body;
+
+    if( !email || !password ||email===""||password===""){
+        return next(errorHandler(400,"All fields are required!"));
+    }
+    try {
+        const validUser=await User.findOne({email});
+
+        if(!validUser){
+            return next(errorHandler(404,"User Not Found"));
+        }
+        const validPassword=bcrypt.compareSync(password,validUser.password);
+
+        if(!validPassword){
+            return next(errorHandler(400,"Wrong credentials"));
+        }
+
+        const token=jwt.sign({id:validUser._id},process.env.JWT_CODE);
+        const {password:pass,...rest}=validUser._doc;
+        res.status(200).cookie("access_token",token,{httpOnly:true}).json(rest);
+    } catch (error) {
+        next(error);
     }
 }
