a39a7f78541942ddc51081cb928ad9b7618ab6a5
Amit Sarkar <mroxygen2000@gmail.com>
2024-12-19 18:53:28 +0530

    get post with search backend

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,7 +1,8 @@
 import express from "express"
-import { create } from "../controllers/post.controller.js";
+import { create, getPosts } from "../controllers/post.controller.js";
 import {verifyToken} from "../utils/verifyUser.js"
 const router=express.Router();
 
 router.post("/create",verifyToken,create)
+router.get("/getPost",getPosts)
 export default router;
